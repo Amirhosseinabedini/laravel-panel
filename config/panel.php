3783ea60b07033bea6e871553250ab4833970796
amir.abedini <abedini@dornica.net>
2022-06-11 16:41:41 +0430

    first commit

---
+++ config/panel.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'date_format'         => 'Y-m-d',
+    'time_format'         => 'H:i:s',
+    'primary_language'    => 'Fa',
+    'available_languages' => [
+        'Fa' => 'farsi',
+        'En' => 'english',
+    ],
+];
